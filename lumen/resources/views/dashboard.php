fa1785cb8db131fe0d3d96d8c5211735f811e990
amshel <amshelhack3r@gmail.com>
2019-09-04 23:10:25 +0000

    add methods for moving images

--- lumen/resources/views/dashboard.php
+++ lumen/resources/views/dashboard.php
@@ -15,8 +15,6 @@ include "partials/header.php";
                         <thead>
                             <tr>
                                 <th>Image</th>
-                                <th>name</th>
-                                <th>Location</th>
                                 <th>Options</th>
                             </tr>
                         </thead>
@@ -24,19 +22,30 @@ include "partials/header.php";
                             <?php
                             foreach ($pictures as $picture) : ?>
                                 <tr>
-                                    <td><img src="<?php echo URL::asset('assets/images/') . '/' . $picture->name; ?>" alt="<?php echo $picture->name; ?> " width="400" height="200"></td>
-                                    <td><?php echo $picture->caption; ?></td>
-                                    <td><?php echo $picture->location; ?></td>
                                     <td>
-                                        <button id="rename" onclick="showRename(this)">RENAME</button>
-                                        <div id="form">
+                                        <img src="<?php echo URL::asset('assets/images/') . '/' . $picture->name; ?>" alt="<?php echo $picture->name; ?> " width="400" height="200">
+                                        <p id="caption_<?php echo $picture->picture; ?>">CAPTION: <?php echo $picture->caption; ?></p>
+                                        <p class="location">LOCATION ON WEBSITE: <?php echo $picture->location; ?></p>
+                                    </td>
+                                    <td>
+                                        <button class="btn btn-primary" onclick="showRename(this)">CHANGE CAPTION</button>
+                                        <div class="form">
                                             <input type="text" name="" id="" placeholder="New caption">
-                                            <button onclick="renameImage(<?php echo $picture->picture; ?>, this.previousElementSibling)">UPDATE</button>
+                                            <button class="btn btn-success" onclick="renameImage(<?php echo $picture->picture; ?>, this.previousElementSibling)">UPDATE</button>
                                         </div>
                                         <br>
                                         <hr>
-                                        <button id="delete" onclick="deleteImage(<?php echo $picture->picture; ?>)">DELETE</button>
-                                    </td>
+                                        <?php
+                                            if($picture->location == "slider"){
+                                                echo "<button onclick = 'moveImage($picture->picture)' class='btn btn-info'>Remove from slider</button>";
+                                            }else{
+                                                echo "<button onclick = 'moveImage($picture->picture)' class='btn btn-info'>Add to slider</button>";
+                                            } 
+                                        ?>
+                                        <br>
+                                        <hr>
+                                        <button class="btn btn-danger" id="delete" onclick="deleteImage(<?php echo $picture->picture; ?>, this.parentNode.parentNode)">DELETE IMAGE</button>
+                                        </td>
                                 </tr>
                             <?php endforeach; ?>
                         </tbody>
@@ -51,37 +60,80 @@ include "partials/header.php";
 <!-- /.content-wrapper -->
 </div>
 <script>
+
     const showRename = (button) => {
-        document.querySelector("#form").style.display = "block";
+        button.nextElementSibling.style.display = "block";
         button.style.display = "none";
     }
     const renameImage = (id, input) => {
-
         if (!input.value)  {
             alert("Cannot be empty")
         } else {
             fetch('/rename', {
                 method: 'post',
+                headers: {
+                'Content-type': 'application/json'
+                },
                 body: JSON.stringify({
-                                        id,
+                                        id:id,
                                         caption: input.value
                                     })
             }).then(response => {
                 return response.text();
             }).then(data =>{
                 if(data == "success"){
-                    document.querySelector("#form").style.display = "none";
-                    document.querySelector("#rename").style.display = "block";
+                    input.parentNode.style.display = "none";
+                    input.parentNode.previousElementSibling.style.display = "block";
+                    document.querySelector(`#caption_${id}`).innerHTML = `CAPTION: ${input.value}`;
                 }else{
                     console.log("not successful");
-                    
                 }
             }).catch(err =>console.log(err));
         }
     };
-    const deleteImage = (id) => {
+    const deleteImage = (id, row) => {
+        if(confirm("You are about to delete this image")){
+            fetch('/delete', {
+                method: 'post',
+                headers: {
+                'Content-type': 'application/json'
+                },
+                body: JSON.stringify({
+                                        id:id,
+                                    })
+            })
+                .then(response =>{
+                    return response.text();
+                })
+                .then(data => {
+                    if(data == "deleted"){
+                        row.style.display = "none";    
+                    }
+                })
+                .catch(err =>console.error(err));
 
+        }
     };
+
+    const moveImage = id => {
+        fetch('/move_image', {
+                method: 'post',
+                headers: {
+                'Content-type': 'application/json'
+                },
+                body: JSON.stringify({
+                                        id,
+                                    })
+            })
+            .then(response => response.text())
+            .then(data => {
+                alert(`Image relocated to ${data}`)
+                document.location.reload();
+            })
+            .catch(err =>console.error(err));
+
+    }
+    
 </script>
 <!-- /#wrapper -->
 <?php
